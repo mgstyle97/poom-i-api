913b4869823b87210dd31d3ccc7a7a452b682793
Mingi Kim <migni4575@gmail.com>
2021-08-09 21:14:39 +0900

    delete repository package

--- src/main/java/io/wisoft/poomi/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package io.wisoft.poomi.repository;
-
-import io.wisoft.poomi.domain.member.address.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- src/main/java/io/wisoft/poomi/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package io.wisoft.poomi.repository;
-
-import io.wisoft.poomi.configures.security.jwt.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-}
