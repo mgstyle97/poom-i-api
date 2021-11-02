62455b3d5934d3d8e30b7da857f71380cd9ca74e
Mingi Kim <migni4575@gmail.com>
2021-11-02 10:47:02 +0900

    modified slack error log

--- src/main/java/io/wisoft/poomi/global/aop/exception/SlackNotificationAspect.java
+++ src/main/java/io/wisoft/poomi/global/aop/exception/SlackNotificationAspect.java
@@ -1,27 +1,36 @@
 package io.wisoft.poomi.global.aop.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.wisoft.poomi.global.dto.response.ApiResponse;
 import io.wisoft.poomi.global.exception.ErrorResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 @Aspect
 public class SlackNotificationAspect {
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Value("${slack.url}")
     private String slackUrl;
@@ -36,22 +45,31 @@ public class SlackNotificationAspect {
             value = "execution(* io.wisoft.poomi.global.exception.RestExceptionHandler.*(..))",
             returning = "errorResponse"
     )
-    public ApiResponse<ErrorResponse> notify(final JoinPoint joinPoint, final ApiResponse<ErrorResponse> errorResponse) {
+    public ApiResponse<ErrorResponse> notify(final JoinPoint joinPoint, final ApiResponse<ErrorResponse> errorResponse)
+            throws JsonProcessingException {
         sendErrorInfo2Slack(errorResponse.getError().getMessage());
 
         return errorResponse;
     }
 
-    private void sendErrorInfo2Slack(final String errorMessage) {
+    private void sendErrorInfo2Slack(final String errorMessage) throws JsonProcessingException {
+        final HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        final Map<String, String[]> requestBody = request.getParameterMap();
+        log.info(mapper.writeValueAsString(request.getParameter("name")));
+
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + slackToken);
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
-        Map<String, Object> request = new HashMap<>();
-        request.put("channel", slackChannel);
-        request.put("text", "[Poom-i]\n" + getCurrentTime() + "\n" + errorMessage);
+        Map<String, Object> messageBody = new HashMap<>();
+        messageBody.put("channel", slackChannel);
+        messageBody.put("text",
+                "[Poom-i]\n" + getCurrentTime() + "\n" + "error_message: " + errorMessage +
+                        "\nrequest_url: " + request.getRequestURL() +
+                        "\nclient_ip: " + getClientIP(request));
 
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(request, headers);
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(messageBody, headers);
 
 
         ResponseEntity<String> response = restTemplate.exchange(slackUrl, HttpMethod.POST, entity, String.class);
@@ -62,6 +80,15 @@ public class SlackNotificationAspect {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
 
         return formatter.format(new Date());
+    }
+
+    private String getClientIP(final HttpServletRequest request) {
+        String ip = request.getHeader("X-FORWARDED-FOR");
+        if (!StringUtils.hasText(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
     }
 
 }
